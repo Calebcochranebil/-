f462e9a5a0228905a106c60ba200c5369cc1a35e
calebcochranebil <calebcochranebil33@gmail.com>
2023-11-13 12:01:04 -0700

    allowedhosts

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['limitless-mountain-86122-0cd0533ba362.herokuapp.com', 'www.yourdomain.com']
 
 
 # Application definition
